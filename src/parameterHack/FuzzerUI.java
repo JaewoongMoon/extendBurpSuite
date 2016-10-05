b9f31af0d0a249e9d63507ff43460f0d8c3b96c6
unknown <jwmoon@ad.datahotel.intranet>
2016-10-05 15:06:16 +0900

    split event handler as a inner class

--- src/parameterHack/FuzzerUI.java
+++ src/parameterHack/FuzzerUI.java
@@ -9,6 +9,7 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.rmi.activation.ActivationSystem;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Vector;
@@ -34,7 +35,7 @@ import javax.swing.JScrollPane;
  * @author	: 문재웅(mjw8585@gmail.com)
  * @Date	: 2016. 10. 4.
  */
-public class FuzzerUI extends JFrame implements ActionListener{
+public class FuzzerUI extends JFrame {
 
 	   
    // Choice fuzzerBox = new Choice();
@@ -69,7 +70,7 @@ public class FuzzerUI extends JFrame implements ActionListener{
     	fuzzers.add(FuzzerType.SQL_INJECTION.toString());
     	
     	fuzzerBox = new JComboBox<>(fuzzers);
-    	fuzzerBox.addActionListener(this);
+    	fuzzerBox.addActionListener(new ConversionActionHandler());
     	
     	
     	// 파라메터 입력 텍스트 Input
@@ -120,7 +121,7 @@ public class FuzzerUI extends JFrame implements ActionListener{
     	label4.setBounds(550, 210, 300, 20);
     	userParamField.setBounds(550, 240, 250, 20);
     	userParamField.setEnabled(false);
-    	userParamField.addActionListener(this);
+    	//userParamField.addActionListener(this);
     	
     	
     	// 컴포넌트 추가 
@@ -142,52 +143,55 @@ public class FuzzerUI extends JFrame implements ActionListener{
     	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     
-    /* (non-Javadoc)
-	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-	 */
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		//System.out.println("**** 선택한 값: " + fuzzerBox.getSelectedItem());
-	
-		// STEP 0. 유저 파라메터 입력 부분 Visible 처리 
-		if (fuzzerBox.getSelectedIndex() == 1){ // Normal 이면 보이게  
-			userParamField.setEnabled(true);
-		}else{
-			userParamField.setEnabled(false);
-			userParamField.setText("");
-		}
-		
-		// STEP 1. 입력된 파라메터 값 얻어오기 
-		// 값 체크 
-		String inputParam = input1.getText();
-		if(inputParam == null || inputParam.equals("")){
-			JOptionPane.showMessageDialog(null, "입력 파라메터 값이 없습니다.");
-			return ;
-		}
-		// (보완) 파라메터 형식에 맞는 지도 체크를 하면 좋을 듯
-		//System.out.println(input1.getText());
-		
-		// STEP 2. 선택 된 Fuzzer Type 변환
-		if (fuzzerBox.getSelectedIndex() == 0){
-			JOptionPane.showMessageDialog(null, "퍼저 타입을 선택해 주십시오.");
-			return ;
-		}	
-		FuzzerType fuzzer = FuzzerType.valueOf(fuzzerBox.getSelectedIndex());
-		
-		// STEP 3. ParameterHack 에게 변환 작업 요청
-		ParameterHack paramHack = new ParameterHack();
-		paramHack.setFuzzer(fuzzer);
-		String paramValue = userParamField.getText() == null? "" : userParamField.getText();
-		String result = paramHack.makeFuzzerString(inputParam, "", paramValue);
-		
-		// STEP 4. 결과 값 출력 
-		output.setText(result);
-		
-	}
+   
 
     public static void main(String[] args){
     	new FuzzerUI();
     }
 
-
+    class ConversionActionHandler implements ActionListener{
+    	
+    	 /* (non-Javadoc)
+    	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+    	 */
+    	@Override
+    	public void actionPerformed(ActionEvent e) {
+    		//System.out.println("**** 선택한 값: " + fuzzerBox.getSelectedItem());
+    	
+    		// STEP 0. 유저 파라메터 입력 부분 Visible 처리 
+    		if (fuzzerBox.getSelectedIndex() == 1){ // Normal 이면 보이게  
+    			userParamField.setEnabled(true);
+    		}else{
+    			userParamField.setEnabled(false);
+    			userParamField.setText("");
+    		}
+    		
+    		// STEP 1. 입력된 파라메터 값 얻어오기 
+    		// 값 체크 
+    		String inputParam = input1.getText();
+    		if(inputParam == null || inputParam.equals("")){
+    			JOptionPane.showMessageDialog(null, "입력 파라메터 값이 없습니다.");
+    			return ;
+    		}
+    		// (보완) 파라메터 형식에 맞는 지도 체크를 하면 좋을 듯
+    		//System.out.println(input1.getText());
+    		
+    		// STEP 2. 선택 된 Fuzzer Type 변환
+    		if (fuzzerBox.getSelectedIndex() == 0){
+    			JOptionPane.showMessageDialog(null, "퍼저 타입을 선택해 주십시오.");
+    			return ;
+    		}	
+    		FuzzerType fuzzer = FuzzerType.valueOf(fuzzerBox.getSelectedIndex());
+    		
+    		// STEP 3. ParameterHack 에게 변환 작업 요청
+    		ParameterHack paramHack = new ParameterHack();
+    		paramHack.setFuzzer(fuzzer);
+    		String paramValue = userParamField.getText() == null? "" : userParamField.getText();
+    		String result = paramHack.makeFuzzerString(inputParam, "", paramValue);
+    		
+    		// STEP 4. 결과 값 출력 
+    		output.setText(result);
+    		
+    	}
+    }
 }
